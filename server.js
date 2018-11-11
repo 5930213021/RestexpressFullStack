67bd81940eba72db273c7e2fd9c65bc89aca75a0
nisarat <nisaratnsaelim@gmail.com>
2018-11-11 21:32:03 +0700

    add

--- server.js
+++ server.js
@@ -46,7 +46,11 @@ app.post('/api/purchases/',db.insertPurchases);
 app.put('/api/purchases/:id',db.updatePurchases);
 app.delete('/api/purchases/:id',db.deletePurchases);
 
-
+app.get('/api/users/',db.getAllUsers);
+app.get('/api/users/:id',db.getUsersByID);
+app.post('/api/users/',db.insertUsers); 
+app.put('/api/users/:id',db.updateUsers);
+app.delete('/api/users/:id',db.deleteUsers);
 
 
 var port = process.env.PORT || 8080;
